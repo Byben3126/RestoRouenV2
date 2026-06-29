6091505b6e8ea39cdcdda52ab5913665d8ee5e2e
Byben3126 <clement.guilloux3126@gmail.com>
2026-06-29 14:08:53 +0200

    Update restaurant.controller.ts

--- apps/app/src/restaurant/restaurant.controller.ts
+++ apps/app/src/restaurant/restaurant.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, UseGuards } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
 import { Serialize } from '@main/common/decorators/serialize.decorator';
 
 import { CurrentUser } from '../common/decorators/current-user.decorator';
+import { AuthGuard } from '../common/guards/auth.guard';
 import { CreateRestaurantDto, RestaurantDto, UpdateRestaurantDto } from './dto';
 import { RestaurantService } from './restaurant.service';
 
 @ApiTags('Restaurant')
+@UseGuards(AuthGuard)
 @Controller('restaurant')
 export class RestaurantController {
   constructor(private readonly restaurantService: RestaurantService) {}
