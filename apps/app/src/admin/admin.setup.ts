145edf232d3875b27991f139aff285c0c57ad8ed
Byben3126 <clement.guilloux3126@gmail.com>
2026-06-30 11:21:44 +0200

    replace dynamic import

--- apps/app/src/admin/admin.setup.ts
+++ apps/app/src/admin/admin.setup.ts
@@ -1,13 +1,12 @@
 import type { INestApplication } from '@nestjs/common';
 
 import { MikroORM } from '@mikro-orm/core';
+import AdminJS from 'adminjs';
+import { Database, Resource } from '@adminjs/mikroorm';
+import AdminJSExpress from '@adminjs/express';
 
 export async function setupAdmin(app: INestApplication): Promise<void> {
   const orm = app.get(MikroORM);
-
-  const AdminJS = (await import('adminjs')).default;
-  const { Database, Resource } = await import('@adminjs/mikroorm');
-  const AdminJSExpress = (await import('@adminjs/express')).default;
 
   AdminJS.registerAdapter({ Database, Resource });
 
